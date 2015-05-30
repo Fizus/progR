3ff6448a3aab49fe8475d16a3354ab94ad2efd84
piotrekwe <piotrekwe@gmail.com>
2015-05-30 23:19:34 +0200

    ciag dlaszy

--- podstawoweklasy.cpp
+++ podstawoweklasy.cpp
@@ -20,9 +20,9 @@ double odchylenie (vector<double> v)
 {
     double s=0.0;
     double sr=srednia(v);
-    for(int i=0;i<v.size();i++)
+    for(int i=0; i<v.size(); i++)
     {
-        s+=(v[i]-sr)*(v[i]-sr);
+        s+=pow((v[i]-sr), 2);
     }
     s=(double)s/(double)v.size();
     s=sqrt(s);
@@ -36,7 +36,7 @@ class histogram
     vector<int>bins;
 public:
     histogram (double a,double b, int n, vector<double> dane); //od a do b n kubelkow,
-    histogram (double o,int n, vector<double> dane); // o sigma od œredniej, n kubelkow
+    histogram (double o, int n, vector<double> dane); // o sigma od sredniej, n kubelkow
     int odczyt (int i); // odczytuje i ty kubelek
     void wyrzut (ostream& out); //wypisuje na output
 };
@@ -45,7 +45,7 @@ void histogram::wyrzut (ostream& out)
 {
     for(int i=0; i<bins.size(); i++)
     {
-        out<<(double)a+(b-a)/(double)bins.size()*((double)i+0.5)<<" "<<bins[i]<<endl;
+        out << (double)a+(b-a)/(double)bins.size()*((double)i+0.5)<<" "<< bins[i] << endl;
     }
 }
 
@@ -54,15 +54,15 @@ histogram::histogram (double o,int n, vector<double> dane)
     int k;
     a=srednia(dane)-o*odchylenie(dane);
     b=srednia(dane)+o*odchylenie(dane);
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         bins.push_back(0);
     }
-    for(int i=0;i<dane.size();i++)
+    for(int i=0; i<dane.size(); i++)
     {
         k=(int)((double)(((double)n*(dane[i]-a)/(b-a))));
-        if(k<n&&k>=0)
-        bins[k]++;
+        if(k<n && k>=0)
+            bins[k]++;
     }
 }
 
